8cb1cde1ba8e0e43506c3952ece6dbe971d2d94b
Vqhuy1212 <vqhuy1212@gmail.com>
2026-05-15 21:49:06 +0700

    LeapYearCalculator

--- src/LeapYearCalculator.java
+++ src/LeapYearCalculator.java
@@ -7,7 +7,7 @@ public class LeapYearCalculator {
         System.out.println("Enter a year: ");
         int year = scanner.nextInt();
 
-        /*
+        /* FIRST WAY
         if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
             System.out.printf("%d is a leap year!", year);
         } else {
@@ -15,6 +15,7 @@ public class LeapYearCalculator {
         }
         */
 
+        /* SECOND WAY
         if (year % 4 == 0) {
             if (year % 100 == 0) {
                 if (year % 400 == 0) {
@@ -25,6 +26,48 @@ public class LeapYearCalculator {
             } else {
                 System.out.printf("%d is a leap year!", year);
             }
+        } else {
+            System.out.printf("%d is NOT a leap year!", year);
+        }
+        */
+
+        /* THIRD WAY
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if (isLeapYear) {
+            System.out.printf("%d is a leap year!", year);
+        } else {
+            System.out.printf("%d is NOT a leap year!", year);
+        }
+        */
+
+        boolean isLeapYear = false;
+
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if(isDivisibleBy4){
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if(isDivisibleBy100){
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if(isDivisibleBy400){
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if (isLeapYear) {
+            System.out.printf("%d is a leap year!", year);
         } else {
             System.out.printf("%d is NOT a leap year!", year);
         }
