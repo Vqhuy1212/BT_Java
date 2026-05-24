06c83ecc2ceeeb1dc539830d1afe99ea9cba129b
Vqhuy1212 <vqhuy1212@gmail.com>
2026-05-24 19:50:47 +0700

    PrimeNumbers

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+
+        while (count < 20) {
+
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(number);
+                count++;
+            }
+
+            number++;
+        }
+    }
+}
