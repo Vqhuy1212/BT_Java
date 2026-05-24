7b828758b126dc1d9f756568055250de89d46338
Vqhuy1212 <vqhuy1212@gmail.com>
2026-05-24 17:37:34 +0700

    Interest

---
+++ src/Interest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Interest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter investment amount: ");
+        double money = scanner.nextDouble();
+
+        System.out.println("Enter number of months: ");
+        int month = scanner.nextInt();
+
+        System.out.println("Enter annual interest rate in percentage: ");
+        double interestRate = scanner.nextDouble();
+
+        double totalInterest = 0;
+        for(int i = 0; i < month; i++){
+            totalInterest += money * (interestRate / 100) / 12;
+        }
+
+        System.out.println("Total of interest: " + totalInterest);
+    }
+
+}
